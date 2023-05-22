47d371b0a0404ddf724b2af66246e9e18aaea199
D.Visaggi <115939571+DanielVisaggi@users.noreply.github.com>
2023-05-22 16:18:52 -0300

    Add files via upload

---
+++ danicarp/src/main/java/com/example/danicarp/DanicarpApplication.java
@@ -0,0 +1,13 @@
+package com.example.danicarp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanicarpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanicarpApplication.class, args);
+	}
+
+}
---
+++ danicarp/src/test/java/com/example/danicarp/DanicarpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danicarp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanicarpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
